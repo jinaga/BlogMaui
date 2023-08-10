3460e822f0045aafc13e451ab3c1851f9c0fcfde
Michael L Perry <michael@qedcode.com>
2023-08-09 21:24:02 -0500

    First MAUI app with Jinaga.

---
+++ MauiApp1/Blog/Model.cs
@@ -0,0 +1,15 @@
+﻿using Jinaga;
+
+namespace MauiApp1.Blog;
+
+[FactType("Blog.Site")]
+public record Site(string domain) { }
+
+[FactType("Blog.Post")]
+public record Post(Site site, DateTime createdAt) { }
+
+[FactType("Blog.Post.Title")]
+public record PostTitle(Post post, string value, PostTitle[] prior) { }
+
+[FactType("Blog.Post.Deleted")]
+public record PostDeleted(Post post, DateTime deletedAt) { }
