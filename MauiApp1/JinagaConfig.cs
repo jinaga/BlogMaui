469d8f19b9d8e293af7194259e03f8707db56063
Michael L Perry <michael@qedcode.com>
2023-08-17 07:53:22 -0500

    Changed name to JinagaConfig

--- MauiApp1/JinagaConfig.cs
+++ MauiApp1/JinagaConfig.cs
@@ -3,7 +3,7 @@
 namespace MauiApp1;
 internal static class JinagaConfig
 {
-    public static Jinaga.Jinaga j = JinagaClient.Create(opt =>
+    public static JinagaClient j = JinagaClient.Create(opt =>
     {
         opt.HttpEndpoint = new Uri("https://repdev.jinaga.com/N25EVWOs91edOIao79xosTUjEpDHF4HrxOx0GrpZtbMq3ZHqu7DyeiDmEgmhnbBLTdQCBS79OzdzOzTRLi54VQ");
     });
