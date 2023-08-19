47978134fd301f6742ff4e266788ea343669f6aa
Michael L Perry <michael@qedcode.com>
2023-08-18 22:13:11 -0500

    Set configuration

--- BlogMaui/JinagaConfig.cs
+++ BlogMaui/JinagaConfig.cs
@@ -1,10 +1,13 @@
 ﻿using Jinaga;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 namespace BlogMaui;
 internal static class JinagaConfig
 {
     public static JinagaClient j = JinagaClient.Create(opt =>
     {
-        opt.HttpEndpoint = new Uri("https://repdev.jinaga.com/N25EVWOs91edOIao79xosTUjEpDHF4HrxOx0GrpZtbMq3ZHqu7DyeiDmEgmhnbBLTdQCBS79OzdzOzTRLi54VQ");
+        var settings = new Settings();
+        opt.HttpEndpoint = new Uri(settings.ReplicatorUrl);
     });
 }
---
+++ BlogMaui/Settings.cs
@@ -0,0 +1,15 @@
+﻿namespace BlogMaui;
+internal partial class Settings
+{
+    // Create a file named Settings.Local.cs
+    // Set the properties in the Settings constructor
+    // Do not check Settings.Local.cs into source control
+
+    // For example:
+    //
+    // public Settings()
+    // {
+    //     ReplicatorUrl = "https://repdev.jinaga.com/xxxXXXXxxxyyyyYYYyyy";
+    // }
+    public string ReplicatorUrl { get; } = "https://repdev.jinaga.com/yourreplicator";
+}
