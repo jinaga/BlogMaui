847a8cba1efb14ef4f8a863856606610cf83b877
Michael L Perry <michael@qedcode.com>
2023-08-20 20:29:37 -0500

    Configure the callback scheme for Android

---
+++ BlogMaui/Authentication/WebAuthenticationCallbackActivity.cs
@@ -0,0 +1,22 @@
+﻿#if ANDROID
+
+using Android.App;
+using Android.Content.PM;
+
+//
+// For complete instructions, please see:
+// https://learn.microsoft.com/en-us/dotnet/maui/platform-integration/communication/authentication?tabs=android
+//
+
+namespace BlogMaui.Authentication;
+
+[Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+[IntentFilter(new[] { Android.Content.Intent.ActionView },
+              Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
+              DataScheme = CALLBACK_SCHEME)]
+public class WebAuthenticationCallbackActivity : WebAuthenticatorCallbackActivity
+{
+    const string CALLBACK_SCHEME = "blogmaui";
+}
+
+#endif
