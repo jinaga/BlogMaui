660dfffbd2d92f6ba88356a27bd2bb6277e98eb1
Michael L Perry <michael@qedcode.com>
2023-10-21 16:00:10 -0500

    Move the login command to the app shell view model

--- BlogMaui/AppShellViewModel.cs
+++ BlogMaui/AppShellViewModel.cs
@@ -1,10 +1,45 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
 
-namespace BlogMaui
+namespace BlogMaui;
+
+public partial class AppShellViewModel : ObservableObject
 {
-    public partial class AppShellViewModel : ObservableObject
+    private readonly OAuth2HttpAuthenticationProvider authenticationProvider;
+
+    [ObservableProperty]
+    private string appState = "Initializing";
+    [ObservableProperty]
+    private string error = string.Empty;
+
+    public ICommand LogIn { get; }
+
+    public AppShellViewModel(OAuth2HttpAuthenticationProvider authenticationProvider)
     {
-        [ObservableProperty]
-        private string appState = "Initializing";
+        this.authenticationProvider = authenticationProvider;
+
+        LogIn = new AsyncRelayCommand(HandleLogIn);
+    }
+
+    private async Task HandleLogIn()
+    {
+        try
+        {
+            Error = string.Empty;
+            bool loggedIn = await authenticationProvider.Login();
+
+            if (loggedIn)
+            {
+                AppState = "LoggedIn";
+
+                // Use two slashes to prevent back navigation.
+                await Shell.Current.GoToAsync("//loggedin");
+            }
+        }
+        catch (Exception ex)
+        {
+            Error = $"Error while logging in: {ex.GetMessage()}";
+        }
     }
 }
--- BlogMaui/Visitor/VisitorViewModel.cs
+++ BlogMaui/Visitor/VisitorViewModel.cs
@@ -1,45 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
-using System.Windows.Input;
 
 namespace BlogMaui.Visitor;
 
 public partial class VisitorViewModel : ObservableObject
 {
-    private readonly OAuth2HttpAuthenticationProvider authenticationProvider;
-    private readonly AppShellViewModel appShellViewModel;
-
-    [ObservableProperty]
-    private string error = string.Empty;
-
-    public VisitorViewModel(OAuth2HttpAuthenticationProvider authenticationProvider, AppShellViewModel appShellViewModel)
-    {
-        this.authenticationProvider = authenticationProvider;
-        this.appShellViewModel = appShellViewModel;
-
-        LogIn = new AsyncRelayCommand(HandleLogIn);
-    }
-
-    public ICommand LogIn { get; }
-
-    private async Task HandleLogIn()
-    {
-        try
-        {
-            Error = string.Empty;
-            bool loggedIn = await authenticationProvider.Login();
-
-            if (loggedIn)
-            {
-                appShellViewModel.AppState = "LoggedIn";
-
-                // Use two slashes to prevent back navigation.
-                await Shell.Current.GoToAsync("//loggedin");
-            }
-        }
-        catch (Exception ex)
-        {
-            Error = $"Error while logging in: {ex.GetMessage()}";
-        }
-    }
 }
