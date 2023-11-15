0064d115b8d3033b4de67562d74210dd75668ad9
Michael L Perry <michael@qedcode.com>
2023-11-14 22:18:59 -0600

    Use the production settings in TestFlight

---
+++ Settings/Settings.Production.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogMaui;
+
+// Settings used for the production release.
+partial class Settings
+{
+    public Settings()
+    {
+        ReplicatorUrl = "https://repdev.jinaga.com/N25EVWOs91edOIao79xosTUjEpDHF4HrxOx0GrpZtbMq3ZHqu7DyeiDmEgmhnbBLTdQCBS79OzdzOzTRLi54VQ";
+        AuthUrl = "https://repdev.jinaga.com/N25EVWOs91edOIao79xosTUjEpDHF4HrxOx0GrpZtbMq3ZHqu7DyeiDmEgmhnbBLTdQCBS79OzdzOzTRLi54VQ/auth/apple";
+        AccessTokenUrl = "https://repdev.jinaga.com/N25EVWOs91edOIao79xosTUjEpDHF4HrxOx0GrpZtbMq3ZHqu7DyeiDmEgmhnbBLTdQCBS79OzdzOzTRLi54VQ/auth/token";
+        ClientId = "N25EVWOs91edOIao79xosTUjEpDHF4HrxOx0GrpZtbMq3ZHqu7DyeiDmEgmhnbBLTdQCBS79OzdzOzTRLi54VQ";
+    }
+}
