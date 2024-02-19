65e25a1f958cf119727633584dc663aded55a7b3
Michael L Perry <michael@qedcode.com>
2024-02-18 18:28:10 -0600

    Jinaga 0.11.1

--- BlogMaui/Areas/Blog/Model.cs
+++ BlogMaui/Areas/Blog/Model.cs
@@ -3,25 +3,25 @@
 namespace BlogMaui.Areas.Blog;
 
 [FactType("Blog.User.Name")]
-public partial record UserName(User user, string value, UserName[] prior) { }
+public record UserName(User user, string value, UserName[] prior) { }
 
 [FactType("Blog.Site")]
-public partial record Site(User creator, DateTime createdAt) { }
+public record Site(User creator, DateTime createdAt) { }
 
 [FactType("Blog.Site.Name")]
-public partial record SiteName(Site site, string value, SiteName[] prior) { }
+public record SiteName(Site site, string value, SiteName[] prior) { }
 
 [FactType("Blog.Site.Domain")]
-public partial record SiteDomain(Site site, string value, SiteDomain[] prior) { }
+public record SiteDomain(Site site, string value, SiteDomain[] prior) { }
 
 [FactType("Blog.Post")]
-public partial record Post(Site site, User author, DateTime createdAt) { }
+public record Post(Site site, User author, DateTime createdAt) { }
 
 [FactType("Blog.Post.Title")]
-public partial record PostTitle(Post post, string value, PostTitle[] prior) { }
+public record PostTitle(Post post, string value, PostTitle[] prior) { }
 
 [FactType("Blog.Post.Deleted")]
-public partial record PostDeleted(Post post, DateTime deletedAt) { }
+public record PostDeleted(Post post, DateTime deletedAt) { }
 
 [FactType("Blog.Post.Publish")]
-public partial record Publish(Post post, DateTime date) { }
+public record Publish(Post post, DateTime date) { }
