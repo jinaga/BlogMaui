3ce2ab6e7c530e0d68e284bcb724d5ac5fd7999a
Michael L Perry <michael@qedcode.com>
2024-07-08 22:23:40 -0500

    Fix IoC

--- BlogMaui/Jinaga.Maui/ConfigurationExtensions.cs
+++ BlogMaui/Jinaga.Maui/ConfigurationExtensions.cs
@@ -1,11 +1,14 @@
 ﻿using Jinaga.Http;
 using Jinaga.Maui.Authentication;
+using Jinaga.Maui.Binding;
 
 namespace Jinaga.Maui;
 public static class ConfigurationExtensions
 {
     public static IServiceCollection AddJinagaAuthentication(this IServiceCollection services)
     {
+        services.AddSingleton<ITokenStorage, SecureTokenStorage>();
+        services.AddSingleton<UserProvider>();
         services.AddSingleton<AuthenticationProviderProxy>();
         services.AddSingleton<IHttpAuthenticationProvider>(
             s => s.GetRequiredService<AuthenticationProviderProxy>());
